9038c1cf1df723fd5418909377da15693b6b5006
ssawan2 <ssawan2@uwo.ca>
2020-12-29 03:06:14 -0500

    Adding the hidden feature for reviews

--- src/app/components/admin-access/admin-access.component.ts
+++ src/app/components/admin-access/admin-access.component.ts
@@ -5,6 +5,8 @@ import { AuthService } from 'src/app/service/auth.service';
 
 import { User } from "../../shared/services/user";
 import { FormControl, FormGroup } from "@Angular/forms";
+import { ReviewService } from 'src/app/service/review.service';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-admin-access',
   templateUrl: './admin-access.component.html',
@@ -12,12 +14,13 @@ import { FormControl, FormGroup } from "@Angular/forms";
 })
 export class AdminAccessComponent implements OnInit {
 
-  constructor(public afs: AngularFirestore, private authService: AuthService) { }
+  constructor(public afs: AngularFirestore, private authService: AuthService, public reviewService: ReviewService) { }
   users: User[]
+  reviews: any;
   returnArray: any [] = [];
   editState: boolean = false;
   userToEdit: User
-
+display = false;
   
   ngOnInit(): void {
     this.afs
@@ -27,6 +30,7 @@ export class AdminAccessComponent implements OnInit {
       ss.docs.forEach((doc) => {
       this.returnArray.push(doc.data());
     });
+    this.getReviewList();
   });
   }
   form = new FormGroup({
@@ -49,11 +53,17 @@ export class AdminAccessComponent implements OnInit {
         console.log(e);
     })
   }
-  // updateActive(siteManager: boolean) {
-  //   this.reviewService
-  //     .updateUser(this.user.key, { active: siteManager })
-  //     .catch(err => console.log(err));
-  // }  
+  getReviewList(){
+    this.reviewService.getReviewList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(reviews => {
+      this.reviews = reviews;
+    });
+  }
 
 
 
